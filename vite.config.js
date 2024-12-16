992d3091d8a6ecdaa94dbff2a0770759767c2072
GameMinimalist <128192751+GameMinimalist@users.noreply.github.com>
2024-12-16 13:22:40 +0000

    Fix deployment config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/word-matching-game/'
+  base: '/word-matching-game/',
+  build: {
+    outDir: 'dist'
+  }
 })
