7ae8317ccf71b0c2d55ee382eca9582b993c1127
GameMinimalist <128192751+GameMinimalist@users.noreply.github.com>
2024-12-16 13:29:43 +0000

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/word-matching-game/',
   build: {
-    assetsDir: 'assets',
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   }
 })
